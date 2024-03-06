dbfe484c2cbf4d53d6f16f6ce7a69d38f8f26456
lewis <lewiskinyuanjue.ke@gmail.com>
2024-03-06 10:11:32 +0300

    fixed profile bug

--- main/models.py
+++ main/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
 
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
-    image=models.ImageField(default='default.png',upload_to='images')
+    image=models.ImageField( default='default.png', upload_to='images')
 
 
     
